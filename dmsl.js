136b151786d321862e7aaae52694de93c66c2613
kinghez <58810731+kinghez@users.noreply.github.com>
2024-09-03 13:15:42 +0100

    Create dmsl.js

---
+++ dmsl.js
@@ -0,0 +1,20 @@
+let navbar = document.querySelector(".fa");
+
+let ul = document.querySelector(" #navbar ul");
+
+let non = true; 
+
+function disp(event){
+    if(non === true){
+       
+        ul.style.display = "block";
+        non = false;
+    }
+    else{
+        ul.style.display = "none";
+        non = true;
+    }
+    
+}
+
+navbar.addEventListener("click", disp);
